514c0f200b0d74a7989fb6b79f3e34a47448c8e8
zwj4031 <dnyl@qq.com>
2022-03-04 20:22:40 +0800

    add bak.sh

---
+++ bak.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+mkdir /mnt/s/mystore
+mkdir /mnt/m/mystore/
+mkdir /mnt/r/mystore/
+cp -r ~/git/mystore/* /mnt/s/mystore/
+cp -r ~/git/mystore/* /mnt/m/mystore/
+cp -r ~/git/mystore/* /mnt/r/mystore/
+cp -r /mnt/s/git/mystore/* /mnt/s/mystore/
+cp -r /mnt/s/git/mystore/* /mnt/m/mystore/
+cp -r /mnt/s/git/mystore/* /mnt/r/mystore/
